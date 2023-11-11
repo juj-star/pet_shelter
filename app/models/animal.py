f36ca3fc67a18d24880b42eb75a7e2b7ff221104
Jason Ju <jasju@microsoft.com>
2023-11-10 22:28:15 -0800

    animal

--- app/models/animal.py
+++ app/models/animal.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from bson import ObjectId
 
 class AnimalProfile:
-    def __init__(self, profile_id, type_name, breed_name, dispositions, pic, availability, description, date_created=None):
+    def __init__(self, profile_id, type_name, breed_name, dispositions, pic, availability, description, adoption_hooman=None, date_created=None):
         self._id = ObjectId(profile_id) if profile_id else ObjectId()
         self.type_name = type_name  # e.g., "Dog", "Cat", "Other"
         self.breed_name = breed_name  # e.g., "Golden Retriever", "Other"
@@ -10,11 +10,11 @@ class AnimalProfile:
         self.pic = pic  # URL or path to the picture
         self.availability = availability  # e.g., "Available"
         self.description = description
+        self.adoption_hooman = adoption_hooman  # The string ID of the hooman who adopted the animal
         self.date_created = date_created or datetime.utcnow()  # Set current time if not provided
 
     def to_document(self):
         # Convert instance variables to a dictionary for MongoDB insertion
-        # Convert ObjectId to string and datetime to a string if required
         document = {
             '_id': str(self._id),
             'type_name': self.type_name,
@@ -23,6 +23,7 @@ class AnimalProfile:
             'pic': self.pic,
             'availability': self.availability,
             'description': self.description,
+            'adoption_hooman': self.adoption_hooman,  # Store the string ID directly
             'date_created': self.date_created.isoformat() if isinstance(self.date_created, datetime) else self.date_created
         }
         return document
@@ -36,4 +37,5 @@ class AnimalProfile:
         print(f"Picture URL: {self.pic}")
         print(f"Availability: {self.availability}")
         print(f"Description: {self.description}")
-        print(f"Date Created: {self.date_created}")
+        print(f"Adoption Hooman ID: {self.adoption_hooman if self.adoption_hooman else 'None'}")
+        print(f"Date Created: {self.date_created.isoformat() if isinstance(self.date_created, datetime) else self.date_created}")
